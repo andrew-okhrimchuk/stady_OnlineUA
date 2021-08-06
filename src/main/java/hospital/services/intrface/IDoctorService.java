7936587a9db85fe05031b00b4107c7d99fa0f9b1
Андрей Охримчук <a0972103356@gmail.com>
2021-08-06 07:19:36 +0300

    Work with DBase
    Added Generic in Service

--- src/main/java/hospital/services/intrface/IDoctorService.java
+++ src/main/java/hospital/services/intrface/IDoctorService.java
@@ -2,15 +2,16 @@ package hospital.services.intrface;
 
 import hospital.domain.Doctor;
 import hospital.domain.Patient;
+import hospital.dto.DoctorDTO;
 import hospital.dto.SelectDTO;
 import hospital.dto.UserDTO;
 import hospital.exeption.ServiceExeption;
 
 import java.util.List;
 
-public interface IDoctorService {
-    List<Doctor> getListDoctors(SelectDTO selectDTO) throws ServiceExeption;
-    List<Doctor> getListDoctors() throws ServiceExeption;
-    Doctor saveDoctor(UserDTO userDTO) throws ServiceExeption;
+public interface IDoctorService extends GenericService <Doctor, SelectDTO> {
+    List<Doctor> getAll(SelectDTO selectDTO) throws ServiceExeption;
+    List<Doctor> getAll() throws ServiceExeption;
+    Doctor save(DoctorDTO doctorDTO) throws ServiceExeption;
     Doctor getDoctorById(long id) throws ServiceExeption;
 }
--- src/main/java/hospital/services/intrface/IPatientService.java
+++ src/main/java/hospital/services/intrface/IPatientService.java
@@ -7,9 +7,9 @@ import hospital.exeption.ServiceExeption;
 
 import java.util.List;
 
-public interface IPatientService {
-    List<Patient> getListPatients(SelectDTO selectDTO) throws ServiceExeption;
-    List<Patient> getListDoctors() throws ServiceExeption;
-    Patient savePatient(UserDTO userDTO) throws ServiceExeption;
+public interface IPatientService extends GenericService <Patient, SelectDTO> {
+    List<Patient> getAll(SelectDTO selectDTO) throws ServiceExeption;
+    List<Patient> getAll() throws ServiceExeption;
+    Patient save(UserDTO userDTO) throws ServiceExeption;
     Patient getUserById(long id) throws ServiceExeption;
 }
